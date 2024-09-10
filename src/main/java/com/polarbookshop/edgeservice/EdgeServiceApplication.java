caf3685d9696d808e2da6a2c5bdd21e349f3952a
stk346 <stk346@gmail.com>
2024-09-11 00:05:33 +0900

    feat: ch9.1.2 경로와 술어 정의

--- src/main/java/com/polarbookshop/edgeservice/EdgeServiceApplication.java
+++ src/main/java/com/polarbookshop/edgeservice/EdgeServiceApplication.java
@@ -2,12 +2,29 @@ package com.polarbookshop.edgeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class EdgeServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EdgeServiceApplication.class, args);
-	}
+		ApplicationContext context = SpringApplication.run(EdgeServiceApplication.class, args);
 
+		// Environment 객체를 통해 설정 값에 접근
+		Environment env = context.getEnvironment();
+
+		// 특정 라우트 ID에 대한 URI 및 Predicates 정보 출력
+		String catalogServiceUri = env.getProperty("spring.cloud.gateway.routes[0].uri");
+		String[] catalogServicePredicates = env.getProperty("spring.cloud.gateway.routes[0].predicates[0]", String[].class);
+		System.out.println("Catalog Service URI: " + catalogServiceUri);
+		System.out.println("Catalog Service Predicates: " + Arrays.toString(catalogServicePredicates));
+
+		String orderServiceUri = env.getProperty("spring.cloud.gateway.routes[1].uri");
+		String[] orderServicePredicates = env.getProperty("spring.cloud.gateway.routes[1].predicates[0]", String[].class);
+		System.out.println("Order Service URI: " + orderServiceUri);
+		System.out.println("Order Service Predicates: " + Arrays.toString(orderServicePredicates));
+	}
 }
