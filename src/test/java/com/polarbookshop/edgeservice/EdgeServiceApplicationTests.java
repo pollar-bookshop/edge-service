aa846e502f468a32fc6e1da2c4bfcdc8b2002632
stk346 <stk346@gmail.com>
2024-09-11 02:17:39 +0900

    feat: ch9.4.1 스프링 세션 데이터 레디스를 통한 세션 처리

--- src/test/java/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.java
+++ src/test/java/com/polarbookshop/edgeservice/EdgeServiceApplicationTests.java
@@ -2,12 +2,34 @@ package com.polarbookshop.edgeservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
-@SpringBootTest
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
+)
+@Testcontainers // 테스트 컨테이너의 자동 시작과 종료를 활성화
 class EdgeServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
+	private static final int REDIS_PORT = 6379;
+
+	@Container
+	static GenericContainer<?> redis =
+			new GenericContainer<>(DockerImageName.parse(("redis:7.0")))
+					.withExposedPorts(REDIS_PORT);
+
+	@DynamicPropertySource
+	static void redisProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.redis.host", () -> redis.getHost());
+		registry.add("spring.redis.port", () -> redis.getMappedPort(REDIS_PORT));
 	}
 
+	// 애플리케이션 컨텍스트가 올바르게 로드되었는지, 레디스 연결이 설공적으로 됐는지를 확인하기 위한 테스트로 비어 있다.
+	@Test
+	void verifyThatSpringContextLoads() {
+	}
 }
