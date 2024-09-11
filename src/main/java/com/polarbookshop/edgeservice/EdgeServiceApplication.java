9b8dbdb8088ac29308c27742e58a282ae80d94ca
stk346 <stk346@gmail.com>
2024-09-12 02:21:28 +0900

    refactor: EdgeServiceApplication - 라우팅 관련 설정 주석처리 -> 액추에이터에서 확인 가능

--- src/main/java/com/polarbookshop/edgeservice/EdgeServiceApplication.java
+++ src/main/java/com/polarbookshop/edgeservice/EdgeServiceApplication.java
@@ -11,20 +11,20 @@ import java.util.Arrays;
 public class EdgeServiceApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext context = SpringApplication.run(EdgeServiceApplication.class, args);
+		/*ApplicationContext context = */SpringApplication.run(EdgeServiceApplication.class, args);
 
-		// Environment 객체를 통해 설정 값에 접근
-		Environment env = context.getEnvironment();
-
-		// 특정 라우트 ID에 대한 URI 및 Predicates 정보 출력
-		String catalogServiceUri = env.getProperty("spring.cloud.gateway.routes[0].uri");
-		String[] catalogServicePredicates = env.getProperty("spring.cloud.gateway.routes[0].predicates[0]", String[].class);
-		System.out.println("Catalog Service URI: " + catalogServiceUri);
-		System.out.println("Catalog Service Predicates: " + Arrays.toString(catalogServicePredicates));
-
-		String orderServiceUri = env.getProperty("spring.cloud.gateway.routes[1].uri");
-		String[] orderServicePredicates = env.getProperty("spring.cloud.gateway.routes[1].predicates[0]", String[].class);
-		System.out.println("Order Service URI: " + orderServiceUri);
-		System.out.println("Order Service Predicates: " + Arrays.toString(orderServicePredicates));
+//		// Environment 객체를 통해 설정 값에 접근
+//		Environment env = context.getEnvironment();
+//
+//		// 특정 라우트 ID에 대한 URI 및 Predicates 정보 출력
+//		String catalogServiceUri = env.getProperty("spring.cloud.gateway.routes[0].uri");
+//		String[] catalogServicePredicates = env.getProperty("spring.cloud.gateway.routes[0].predicates[0]", String[].class);
+//		System.out.println("Catalog Service URI: " + catalogServiceUri);
+//		System.out.println("Catalog Service Predicates: " + Arrays.toString(catalogServicePredicates));
+//
+//		String orderServiceUri = env.getProperty("spring.cloud.gateway.routes[1].uri");
+//		String[] orderServicePredicates = env.getProperty("spring.cloud.gateway.routes[1].predicates[0]", String[].class);
+//		System.out.println("Order Service URI: " + orderServiceUri);
+//		System.out.println("Order Service Predicates: " + Arrays.toString(orderServicePredicates));
 	}
 }
