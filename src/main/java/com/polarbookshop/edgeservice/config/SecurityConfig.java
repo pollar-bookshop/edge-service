326d0c0c8632b43ce3e08ed43b0cde1f6b6d0239
stk346 <stk346@gmail.com>
2024-09-17 07:26:37 +0900

    feat: ch11.5.2 인증 흐름 제어

--- src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
+++ src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.polarbookshop.edgeservice.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
@@ -8,6 +10,7 @@ import org.springframework.security.oauth2.client.oidc.web.logout.OidcClientInit
 import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
 import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 
 @EnableWebFluxSecurity
@@ -18,8 +21,16 @@ public class SecurityConfig {
             ServerHttpSecurity http,
             ReactiveClientRegistrationRepository clientRegistrationRepository) {
         return http
-                .authorizeExchange(exchange ->
-                        exchange.anyExchange().authenticated()) // 모든 요청은 인증이 필요하다.
+                .authorizeExchange(exchange -> exchange
+                        // SPA의 정적 리소스에 대한 인증되지 않은 엑세스 허용
+                        .pathMatchers("/", "/*.css", "/*.js", "/favicon.ico").permitAll()
+                        // 카탈로그의 도서에 대한 인증되지 않은 엑세스 허용
+                        .pathMatchers(HttpMethod.GET, "/books/**").permitAll()
+                        .anyExchange().authenticated()) // 모든 요청은 인증이 필요하다.
+                // 사용자가 인증되지 않았기 때문에 예외를 발생할 때 HTTP 401로 응답한다.
+                .exceptionHandling(exceptionHandling ->
+                        exceptionHandling.authenticationEntryPoint(
+                                new HttpStatusServerEntryPoint(HttpStatus.UNAUTHORIZED)))
                 .oauth2Login(Customizer.withDefaults()) // OAuth2/오픈ID 커넥트를 사용한 사용자 인증을 활성화한다.
                 // 로그아웃이 성공적으로 완료되는 경우에 대한 사용자 지정 핸들러를 정의
                 .logout(logout -> logout.logoutSuccessHandler(
