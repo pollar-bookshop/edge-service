6e4f546adb9125b53960617761cdce636ed0045e
stk346 <stk346@gmail.com>
2024-09-17 06:41:09 +0900

    feat: ch11.4.4 인증된 사용자 콘텍스트 검사

--- src/main/java/com/polarbookshop/edgeservice/config/RateLimiterConfig.java
+++ src/main/java/com/polarbookshop/edgeservice/config/RateLimiterConfig.java
@@ -5,12 +5,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import reactor.core.publisher.Mono;
 
+import java.security.Principal;
+
 
 @Configuration
 public class RateLimiterConfig {
 
     @Bean
     public KeyResolver keyResolver() {
-        return exchange -> Mono.just("anonymous"); // 요청에 대한 사용률 제한은 상수 키를 사용해 적용한다.
+        // 요청에 대한 사용률 제한은 상수 키를 사용해 적용한다.
+        return exchange -> exchange.getPrincipal()
+                .map(Principal::getName)
+                .defaultIfEmpty("anonymous");
     }
 }
