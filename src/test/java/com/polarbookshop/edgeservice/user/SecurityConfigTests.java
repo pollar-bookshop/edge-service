9c73331c62c6706451470fa359e92572069b8c21
stk346 <stk346@gmail.com>
2024-09-18 04:03:44 +0900

    feat: ch11.6.2 CSRF 테스트

---
+++ src/test/java/com/polarbookshop/edgeservice/user/SecurityConfigTests.java
@@ -0,0 +1,51 @@
+package com.polarbookshop.edgeservice.user;
+
+import com.polarbookshop.edgeservice.config.SecurityConfig;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+@WebFluxTest
+@Import(SecurityConfig.class)
+public class SecurityConfigTests {
+
+    @Autowired
+    WebTestClient webClient;
+
+    @MockBean
+    ReactiveClientRegistrationRepository clientRegistrationRepository;
+
+    // /logout에 HTTP POST 요청을 보내면 HTTP302 응답을 받는지 테스트
+    @Test
+    void whenLogoutAuthenticatedAndWithCsrfTokenThen302() {
+        Mockito.when(clientRegistrationRepository.findByRegistrationId("test"))
+                .thenReturn(Mono.just(testClientRegistration()));
+
+        webClient
+                .mutateWith(SecurityMockServerConfigurers.mockOidcLogin())
+                .mutateWith(SecurityMockServerConfigurers.csrf())
+                .post()
+                .uri("/logout")
+                .exchange()
+                .expectStatus().isFound();
+    }
+
+    private ClientRegistration testClientRegistration() {
+        return ClientRegistration.withRegistrationId("test")
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .clientId("test")
+                .authorizationUri("https://sso.polarbookshop.com/auth")
+                .tokenUri("https://sso.polarbookshop.com/token")
+                .redirectUri("https://polarbookshop.com")
+                .build();
+    }
+}
