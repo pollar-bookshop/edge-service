37991e59ea8bb7dd48d0baa19faad5e30b16be76
stk346 <stk346@gmail.com>
2024-09-20 15:52:42 +0900

    refactor: SecurityConfig - 스프링 시큐리티 6.0에 맞게 설정 (이전 설정대로면 제대로 동작하지 않음)

--- src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
+++ src/main/java/com/polarbookshop/edgeservice/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.polarbookshop.edgeservice.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.Customizer;
@@ -19,6 +20,7 @@ import org.springframework.security.web.server.csrf.CsrfToken;
 import org.springframework.web.server.WebFilter;
 import reactor.core.publisher.Mono;
 
+@Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
 
